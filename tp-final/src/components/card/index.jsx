87e09e6461676ece2d997a715c02d5da8e9eda1d
santi <santiago_abregu_@hotmail.com>
2023-07-12 19:12:43 -0300

    Feat Card

--- tp-final/src/components/card/index.jsx
+++ tp-final/src/components/card/index.jsx
@@ -4,30 +4,52 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import ButtonOutlined from "../buttonOutlined";
+import Signo from "../../img/signo.png";
+import Lagarto from "../../img/lagarto.png";
+import Papel from "../../img/papel.png";
+import Piedra from "../../img/piedra.png";
+import Spock from "../../img/spock.png";
+import Tijera from "../../img/tijera.png";
 
-const MediaCard = ({ title, description, onClickButton, titleButton }) => {
+const MediaCard = ({ title, description, children, image }) => {
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card
+      sx={{
+        maxWidth: 345,
+        backgroundColor: "white",
+        borderRadius: "5px",
+        border: "2px solid black",
+      }}
+    >
       <CardMedia
-        sx={{ height: 140 }}
-        image="/static/images/cards/contemplative-reptile.jpg"
-        title="green iguana"
-      />
+        sx={{ height: 200 }}
+        image={
+          image === "piedra"
+            ? Piedra
+            : image === "papel"
+            ? Papel
+            : image === "tijera"
+            ? Tijera
+            : image === "lagarto"
+            ? Lagarto
+            : image === "spock"
+            ? Spock
+            : Signo
+        }
+        title="eleccion"
+      >
+        {" "}
+      </CardMedia>
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
           {title}
         </Typography>
+
         <Typography variant="body2" color="text.secondary">
           {description}
         </Typography>
       </CardContent>
-      <CardActions>
-        <ButtonOutlined
-          onClick={onClickButton}
-          title={titleButton}
-        ></ButtonOutlined>
-      </CardActions>
+      <CardActions>{children}</CardActions>
     </Card>
   );
 };
